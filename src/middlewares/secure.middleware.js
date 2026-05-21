23dbb11915073344aa9e22bed907de6f39faa284
Clauderson <INCLUSIVAULA@gmail.com>
2026-05-21 09:30:34 -0300

    fix: secureMiddleware usando req.schoolId corretamente

--- src/middlewares/secure.middleware.js
+++ src/middlewares/secure.middleware.js
@@ -1,5 +1,4 @@
 export const secureMiddleware = (req, res, next) => {
-
   if (!req.user) {
     return res.status(401).json({
       success: false,
@@ -7,14 +6,12 @@ export const secureMiddleware = (req, res, next) => {
     });
   }
 
-  if (!req.user?.school_id) {
+  if (!req.schoolId) {
     return res.status(403).json({
       success: false,
       error: "Escola não identificada"
     });
   }
-
-  req.schoolId = req.user.school_id;
 
   next();
 };
